1ad8a554e8ec6037af74514cea7a2835dccc9ea6
pleyston74 <dufourgkevin74@gmail.com>
2021-12-06 21:52:35 -0300

    asdsad

---
+++ src/saboreskevin/SaboresKevin.java
@@ -0,0 +1,13 @@
+
+package saboreskevin;
+
+public class SaboresKevin {
+
+    public static void main(String[] args) {
+        
+        String[] sabores = { "Dulce", "Amargo", "Acido", "Umani", "Picante", "Astringente", "Starchy", "Adiposo" };
+        for (int i=0; i <6; i++) {
+            System.out.println("Sabor:" + sabores[i]);
+        }
+    }  
+}
